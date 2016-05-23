d2b663c151f3fc979412a42ddd0375d0d1a375f7
Mathieu Bordas <mathieu.bordas@smartgridenergy.fr>
2016-05-23 12:06:32 +0200

    fix: Base64Image / copyrights

--- src/main/java/qualify/doc/Base64Image.java
+++ src/main/java/qualify/doc/Base64Image.java
@@ -1,7 +1,16 @@
-/*
- * Copyright (c) 2012-2016 Smart Grid Energy
- * All Right Reserved
- * http://www.smartgridenergy.fr
+/*Copyright (c) 2016, Mathieu Bordas, Nils Haverland
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1- Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+2- Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+3- Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 package qualify.doc;
@@ -14,9 +23,9 @@ import org.jdom.Element;
 public class Base64Image implements DomElementAble {
 	private static final String TAG_NAME = "base64Image";
 	private static final String CONTENT_NAME = "content";
-	
+
 	private String content = null;
-	
+
 	public Base64Image(String content) {
 		this.content = content;
 	}
